8c5c9029d2eda66db032b56ab48b02fc28ebde7f
karthik kashyap <kashyapkarthik3@gmail.com>
2019-08-01 10:05:34 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,43 @@
+package bo;
+
+public class calculator {
+    private int a;
+    private int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int add(int a,int b){
+        return a+b;
+    }
+
+    public int sub(int a,int b){
+        return a-b;
+    }
+    public int mul(int a,int b){
+        return a*b;
+    }
+
+    public int div(int a,int b){
+        return a/b;
+    }
+
+
+
+
+
+
+}
